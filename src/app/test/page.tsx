a4ff0c58b9d6d1cccf5f78b01c5662baca4d0875
zenvlr <lukasvisser@outlook.de>
2025-03-24 17:09:50 +0100

    test

---
+++ src/app/test/page.tsx
@@ -0,0 +1,34 @@
+'use client';
+import Sidebar from '@/components/layouts/sidebar/sidebar';
+import Footer from '@/components/layouts/footer/footer';
+import { Spotlight } from '@/components/ui/spotlight-new';
+
+export default function Test() {
+  return (
+    <div className="flex min-h-screen bg-gradient-to-br from-zinc-900 to-zinc-800 text-white">
+      {/* Sidebar */}
+      <div className="group">
+        <Sidebar />
+      </div>
+
+      {/* Main Content */}
+      <div className="flex-grow h-screen flex flex-col">
+        {/* Spotlight */}
+        <Spotlight />
+
+        <main className="flex-grow flex flex-col transition-all duration-300">
+          <div className="flex flex-col items-center justify-center h-1/3 mt-10">
+            {' '}
+            <h1 className="font-extrabold text-3xl text-center">THE:MES</h1>
+            <p className='text-sm'>penis</p>
+          </div>
+
+          {/* Footer */}
+          <div className="mt-auto px-4 transition-all duration-300">
+            <Footer />
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+}
