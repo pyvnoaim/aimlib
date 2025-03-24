c29eb5783516c7f367fb0619eb620b6af39d4ebe
pyvnoaim <pyvnovlr@gmail.com>
2025-03-24 09:58:14 +0100

    fixed placeholder sites

--- src/app/themes/page.tsx
+++ src/app/themes/page.tsx
@@ -1,18 +1,33 @@
-import type { Metadata } from 'next';
-import Header from '@/components/layouts/header/header';
+'use client';
+import Sidebar from '@/components/layouts/sidebar/sidebar';
 import Footer from '@/components/layouts/footer/footer';
+import { Spotlight } from '@/components/ui/spotlight-new';
 
-export const metadata: Metadata = {
-  title: 'AIM:LIB - Themes',
-  description: 'AIM:LIB - created by aimers, for aimers',
-};
-
-export default function Themes() {
+export default function Home() {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <Header />
-      <h1 className="font-extrabold text-3xl">AIM:LIB - Themes</h1>
-      <Footer />
+    <div className="flex min-h-screen bg-gradient-to-br from-zinc-900 to-zinc-800 text-white">
+      {/* Sidebar */}
+      <div className="group">
+        <Sidebar />
+      </div>
+
+      {/* Main Content */}
+      <div className="flex-grow h-screen flex flex-col">
+        {/* Spotlight */}
+        <Spotlight />
+
+        <main className="flex-grow flex flex-col transition-all duration-300">
+          <div className="flex flex-col items-center justify-center h-1/3 mt-10">
+            {' '}
+            <h1 className="font-extrabold text-3xl text-center">THE:MES</h1>
+          </div>
+
+          {/* Footer */}
+          <div className="mt-auto px-4 transition-all duration-300">
+            <Footer />
+          </div>
+        </main>
+      </div>
     </div>
   );
 }
