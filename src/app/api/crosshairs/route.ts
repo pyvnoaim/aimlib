15ec477ead49f1bdd6074486d16bbd0f538b8fe6
pyvnoaim <pyvnovlr@gmail.com>
2025-03-25 11:20:16 +0100

    api um crosshairs dynamisch auszulesen

---
+++ src/app/api/crosshairs/route.ts
@@ -0,0 +1,20 @@
+// src/app/api/crosshairs/route.ts
+
+import fs from 'fs';
+import path from 'path';
+
+export const revalidate = 10; // Die Route wird alle 10 Sekunden neu generiert
+
+export async function GET() {
+  const crosshairDirectory = path.join(process.cwd(), 'public/crosshairs');
+  const fileNames = fs.readdirSync(crosshairDirectory);
+
+  // Filter nur .png-Dateien
+  const crosshairs = fileNames.filter((file) => file.endsWith('.png'));
+
+  return new Response(JSON.stringify(crosshairs), {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+}
