6bfe5ad88fab391487d1b7fd5e30b110633dbc90
pyvnoaim <pyvnovlr@gmail.com>
2025-03-25 08:53:53 +0100

    added valorant site

---
+++ src/app/valorant/page.tsx
@@ -0,0 +1,33 @@
+'use client';
+import Sidebar from '@/components/layouts/sidebar/sidebar';
+import Footer from '@/components/layouts/footer/footer';
+import { Spotlight } from '@/components/ui/spotlight-new';
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen bg-gradient-to-br from-zinc-900 to-zinc-800 text-white">
+      {/* Sidebar */}
+      <div className="group">
+        <Sidebar />
+      </div>
+
+      {/* Main Content */}
+      <div className="flex-grow h-screen flex flex-col">
+        {/* Spotlight */}
+        <Spotlight />
+
+        <main className="flex-grow flex flex-col transition-all duration-300">
+          <div className="flex flex-col items-center justify-center h-1/3 mt-10">
+            {' '}
+            <h1 className="font-extrabold text-3xl text-center">VALO:RANT</h1>
+          </div>
+
+          {/* Footer */}
+          <div className="mt-auto px-4 transition-all duration-300">
+            <Footer />
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+}
