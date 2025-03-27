e15b44aa4dce69cc5173fe89ce5143a87e94f93a
pyvnoaim <pyvnovlr@gmail.com>
2025-03-27 09:28:02 +0100

    reset test page

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,31 +1,32 @@
 'use client';
-import { useState } from 'react';
 import Sidebar from '@/components/layouts/sidebar/sidebar';
 import Footer from '@/components/layouts/footer/footer';
 import { Spotlight } from '@/components/ui/spotlight-new';
 
-const TestPage = () => {
-  // Add a function name here
+export default function Test() {
   return (
     <div className="flex min-h-screen bg-gradient-to-br from-zinc-900 to-zinc-800 text-white">
+      {/* Sidebar */}
       <div className="group">
         <Sidebar />
       </div>
 
-      <div className="flex-grow flex flex-col">
+      {/* Main Content */}
+      <div className="flex-grow h-screen flex flex-col">
+        {/* Spotlight */}
         <Spotlight />
 
         <main className="flex-grow flex flex-col transition-all duration-300">
-          <div className="flex flex-col items-center justify-center h-1/3 mt-10">
-            <h1 className="font-extrabold text-3xl text-center">TEST</h1>
+          <div className="flex flex-col items-center justify-center h-1/3">
+            <h1 className="font-extrabold text-3xl text-center">Test</h1>
+          </div>
+
+          {/* Footer */}
+          <div className="mt-auto px-4 transition-all duration-300">
+            <Footer />
           </div>
         </main>
-        <div className="mt-auto px-4 transition-all duration-300">
-          <Footer />
-        </div>
       </div>
     </div>
   );
-};
-
-export default TestPage; // Export the component
+}
