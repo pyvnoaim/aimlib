ed73db36c4ae509e378023e4b339567a3cdf2aed
pyvnoaim <pyvnovlr@gmail.com>
2025-04-01 19:20:14 +0200

    fixed 404 site

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,30 +1,14 @@
-'use client';
-
-import { useState, useEffect } from 'react';
 import { Spotlight } from '@/components/ui/spotlight-new';
 import { BiSolidError, BiSolidHome } from 'react-icons/bi';
 import Link from 'next/link';
 
 export default function NotFound() {
-  const [isMobile, setIsMobile] = useState(false);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth < 768);
-    };
-    handleResize();
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
-
   return (
     <div className="flex min-h-screen bg-gradient-to-br from-zinc-900 to-zinc-800 text-white">
       {/* Main Content */}
       <div className="flex-grow h-screen flex flex-col">
-        {/* Spotlight nur auf großen Bildschirmen */}
-        {!isMobile && <Spotlight />}
+        {<Spotlight />}
 
-        {/* Main Content */}
         <main className="flex-grow flex flex-col transition-all duration-300 items-center justify-center text-center px-4">
           <BiSolidError className="text-6xl text-red-500 mb-4" />
           <h1 className="font-extrabold text-3xl md:text-4xl">
