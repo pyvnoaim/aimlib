13b79d473beec61b118c5a6cd59d82653a62ed89
pyvnoaim <pyvnovlr@gmail.com>
2025-04-02 19:38:56 +0200

    added logout button

---
+++ src/components/ui/buttons/logout-button.tsx
@@ -0,0 +1,28 @@
+import { signOut, useSession } from 'next-auth/react';
+import { FiLogOut } from 'react-icons/fi';
+
+const LogoutButton = () => {
+  const { data: session } = useSession();
+
+  const handleLogout = () => {
+    signOut({ callbackUrl: '/' });
+  };
+
+  return (
+    <>
+      {session ? (
+        <button
+          onClick={handleLogout}
+          className="flex items-center p-2 rounded hover:[&>span]:translate-x-2"
+        >
+          <FiLogOut className="w-4 h-4 flex-shrink-0 text-white" />
+          <span className="ml-3 whitespace-nowrap overflow-hidden opacity-0 group-hover:opacity-100 transition-all duration-300 text-white">
+            Log Out
+          </span>
+        </button>
+      ) : null}
+    </>
+  );
+};
+
+export default LogoutButton;
