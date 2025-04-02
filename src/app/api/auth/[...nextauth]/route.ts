d5202917fa36ff823d9263e0d081e7df7b8ac752
pyvnoaim <pyvnovlr@gmail.com>
2025-04-02 19:40:07 +0200

    fixed route for auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -8,16 +8,11 @@ const handler = NextAuth({
       clientSecret: process.env.DISCORD_CLIENT_SECRET!,
       authorization: {
         params: {
-          scope: 'identify email',
+          scope: 'identify',
         },
       },
     }),
   ],
-  pages: {
-    signIn:
-      'https://discord.com/oauth2/authorize?client_id=1354798478961807450&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi%2Fauth%2Fcallback%2Fdiscord&scope=identify+email', // Customize sign-in page if needed
-    error: '/auth/error', // Optional error page URL
-  },
   callbacks: {
     async jwt({ token, account }) {
       if (account) {
