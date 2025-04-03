ad0c5e6f0c1e57cff920c1ab90fe75ea49800148
pyvnoaim <pyvnovlr@gmail.com>
2025-04-03 12:29:07 +0200

    added session age to 30 days

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -27,6 +27,10 @@ const handler = NextAuth({
       return session;
     },
   },
+  session: {
+    strategy: 'jwt',
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+  },
   secret: process.env.NEXTAUTH_SECRET,
 });
 
