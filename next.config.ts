14474cdbffe2db2870529fc267f8f38b838a3c65
pyvnoaim <pyvnovlr@gmail.com>
2025-04-05 23:56:30 +0200

    added discord as remotepattern

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   output: 'standalone',
   images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.discordapp.com',
+      },
+    ],
     loader: 'akamai',
     path: '/',
   },
