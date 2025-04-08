eb98a96592c473069dbeab36a4129b93e37935c8
pyvnoaim <pyvnovlr@gmail.com>
2025-04-08 08:08:34 +0200

    using db pool to fix connection issues

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,11 +1,14 @@
 import mysql from 'mysql2/promise';
 import { drizzle } from 'drizzle-orm/mysql2';
 
-const connection = await mysql.createConnection({
-  host: '212.132.120.105',
-  user: 'root',
-  password: 'lI6kCZgP',
-  database: 'aimlib',
+const pool = mysql.createPool({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
-export const db = drizzle(connection);
+export const db = drizzle(pool);
