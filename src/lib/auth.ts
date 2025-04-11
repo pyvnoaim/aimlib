607f4744220aa907ec27bef5e50d52756568675d
pyvnoaim <pyvnovlr@gmail.com>
2025-04-11 10:48:58 +0200

    added better session handling

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,6 +3,16 @@ import { DrizzleAdapter } from '@auth/drizzle-adapter';
 import { db } from '@/db/index';
 import Discord from 'next-auth/providers/discord';
 
+declare module 'next-auth' {
+  interface User {
+    role?: string;
+  }
+
+  interface Session {
+    user: User;
+  }
+}
+
 export const { handlers, auth, signIn, signOut } = NextAuth({
   trustHost: true,
   adapter: DrizzleAdapter(db),
@@ -13,4 +23,14 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     }),
   ],
   secret: process.env.AUTH,
+  callbacks: {
+    async session({ session, user }) {
+      session.user.id = user.id;
+      session.user.email = user.email;
+      session.user.name = user.name;
+      session.user.image = user.image;
+      session.user.role = user.role;
+      return session;
+    },
+  },
 });
