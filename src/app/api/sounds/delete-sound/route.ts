da1f34533bc6742555dd2fe450b1045288417407
pyvnoaim <pyvnovlr@gmail.com>
2025-04-14 16:04:57 +0200

    added sound apis

--- src/app/api/sounds/delete-sound/route.ts
+++ src/app/api/sounds/delete-sound/route.ts
@@ -1,43 +1,29 @@
 import { NextRequest, NextResponse } from 'next/server';
-import fs from 'fs';
 import path from 'path';
+import fs from 'fs/promises';
 
 export async function DELETE(req: NextRequest) {
   try {
-    const body = await req.json();
-    const { soundId } = body;
+    const { filename } = await req.json();
 
-    if (!soundId) {
+    if (!filename || typeof filename !== 'string') {
       return NextResponse.json(
-        { message: 'Sound ID is required' },
+        { message: 'Filename is required' },
         { status: 400 }
       );
     }
 
-    if (soundId.includes('/') || soundId.includes('\\')) {
-      return NextResponse.json(
-        { message: 'Invalid sound ID' },
-        { status: 400 }
-      );
-    }
+    const filePath = path.join(process.cwd(), 'public', 'sounds', filename);
+    await fs.unlink(filePath);
 
-    const soundFilePath = path.join(process.cwd(), 'public', 'sounds', soundId);
-
-    console.log(soundFilePath);
-
-    if (fs.existsSync(soundFilePath)) {
-      fs.unlinkSync(soundFilePath);
-      return NextResponse.json(
-        { message: 'Sound deleted successfully' },
-        { status: 200 }
-      );
-    } else {
-      return NextResponse.json({ message: 'Sound not found' }, { status: 404 });
-    }
-  } catch (error) {
-    console.error('Error deleting sound:', error);
     return NextResponse.json(
-      { message: 'Failed to delete sound' },
+      { message: 'Sound deleted successfully' },
+      { status: 200 }
+    );
+  } catch (err) {
+    console.error('Error deleting file:', err);
+    return NextResponse.json(
+      { message: 'Failed to delete file' },
       { status: 500 }
     );
   }
