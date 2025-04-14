cf7cd3d9dbb8a56ce26911242e2c619f4eb3c751
pyvnoaim <pyvnovlr@gmail.com>
2025-04-14 16:05:16 +0200

    updated sound page

--- src/app/(pages)/sounds/page.tsx
+++ src/app/(pages)/sounds/page.tsx
@@ -1,59 +1,218 @@
 'use client';
+import { useState, useEffect } from 'react';
+import { useSession } from 'next-auth/react';
 import Sidebar from '@/components/layouts/sidebar/sidebar';
 import Footer from '@/components/layouts/footer/footer';
 import { Spotlight } from '@/components/ui/spotlight-new';
-import { LuDownload } from 'react-icons/lu';
-import { useEffect, useState } from 'react';
+import {
+  FaPlay,
+  FaPause,
+  FaDownload,
+  FaTrash,
+  FaSortAlphaDown,
+  FaSortAlphaUp,
+  FaHeart,
+} from 'react-icons/fa';
 import { BiSearch } from 'react-icons/bi';
-import { FaPlay, FaPause } from 'react-icons/fa';
+import Toast from '@/components/layouts/toast/toast';
+import { ROLES } from '@/types/role';
+import ConfirmDialog from '@/components/layouts/dialog/confirm-dialog';
+
+type Sound = {
+  fullName: string;
+  name: string;
+  fileUrl: string;
+  submittedBy: string;
+  likes: number;
+  isLiked: boolean;
+};
 
 export default function Sounds() {
-  const [sounds, setSounds] = useState<string[]>([]);
-  const [search, setSearch] = useState('');
+  const { data: session } = useSession();
+  const [sounds, setSounds] = useState<Sound[]>([]);
   const [loading, setLoading] = useState(true);
-  const [playingSound, setPlayingSound] = useState<string | null>(null);
-  const [audio, setAudio] = useState<HTMLAudioElement | null>(null);
+  const [search, setSearch] = useState('');
+  const [currentlyPlayingId, setCurrentlyPlayingId] = useState<string | null>(
+    null
+  );
+  const [toast, setToast] = useState<{
+    message: string;
+    type: 'success' | 'error' | 'info';
+    isVisible: boolean;
+  }>({
+    message: '',
+    type: 'info',
+    isVisible: false,
+  });
+  const [isConfirmDialogOpen, setIsConfirmDialogOpen] = useState(false);
+  const [soundToDelete, setSoundToDelete] = useState<Sound | null>(null);
+
+  const [sortBy, setSortBy] = useState(['name', 'submittedBy']);
+  const [sortOrder, setSortOrder] = useState(['asc', 'asc']);
+
+  const handleCloseToast = () => {
+    setToast((prev) => ({
+      ...prev,
+      isVisible: false,
+    }));
+  };
+
+  const showToast = (message: string, type: 'success' | 'error' | 'info') => {
+    setToast({
+      message,
+      type,
+      isVisible: true,
+    });
+  };
 
   useEffect(() => {
-    async function fetchSounds() {
-      const response = await fetch('../api/sounds');
-      const data = await response.json();
-      setSounds(data);
-      setLoading(false);
-    }
+    const fetchSounds = async () => {
+      try {
+        const response = await fetch('/api/sounds/get-sounds');
+        const data = await response.json();
+
+        const soundFiles = data.map((fileName: string) => ({
+          fullName: fileName,
+          name: fileName.replace('.ogg', ''),
+          fileUrl: `/sounds/${fileName}`,
+          submittedBy: 'System',
+          likes: 0,
+          isLiked: false,
+        }));
+
+        setSounds(soundFiles);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching sounds:', error);
+        showToast('Failed to load sounds', 'error');
+        setLoading(false);
+      }
+    };
+
     fetchSounds();
   }, []);
 
-  const handlePlayPause = (sound: string) => {
-    if (playingSound === sound) {
-      audio?.pause();
-      setPlayingSound(null);
-    } else {
-      if (audio) {
-        audio.pause();
+  const playSound = (fileUrl: string, id: string) => {
+    const audio = new Audio(fileUrl);
+    setCurrentlyPlayingId(id);
+    audio.play();
+
+    audio.onended = () => {
+      setCurrentlyPlayingId(null);
+    };
+  };
+
+  const handleDelete = (sound: Sound) => {
+    if (session?.user.role !== ROLES.ADMIN) {
+      showToast('You do not have permission to delete this sound.', 'error');
+      return;
+    }
+
+    setSoundToDelete(sound);
+    setIsConfirmDialogOpen(true);
+  };
+
+  const confirmDelete = async () => {
+    if (soundToDelete) {
+      try {
+        const response = await fetch('/api/sounds/delete-sound', {
+          method: 'DELETE',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ filename: soundToDelete.fullName }),
+        });
+
+        const result = await response.json();
+
+        if (response.ok) {
+          setSounds((prev) =>
+            prev.filter((sound) => sound.fullName !== soundToDelete.fullName)
+          );
+          showToast(
+            `Sound "${soundToDelete.name}" deleted successfully`,
+            'success'
+          );
+        } else {
+          showToast(result.message || 'Failed to delete sound', 'error');
+        }
+      } catch (error) {
+        console.error('Error deleting sound:', error);
+        showToast('Failed to delete sound', 'error');
       }
-      const newAudio = new Audio(`/sounds/${sound}`);
-      newAudio.play();
-      setAudio(newAudio);
-      setPlayingSound(sound);
-      newAudio.onended = () => setPlayingSound(null);
+
+      setIsConfirmDialogOpen(false);
+      setSoundToDelete(null);
     }
   };
 
-  const filteredSounds = sounds.filter((sound) => {
-    const title = sound.replace('.ogg', '');
-    return title.toLowerCase().includes(search.toLowerCase());
+  const cancelDelete = () => {
+    setIsConfirmDialogOpen(false);
+    setSoundToDelete(null);
+  };
+
+  const handleDownload = (fileUrl: string) => {
+    const link = document.createElement('a');
+    link.href = fileUrl;
+    link.download = fileUrl.split('/').pop() || 'sound.ogg';
+    link.click();
+    showToast('Downloading sound...', 'info');
+  };
+
+  const handleLikeToggle = (sound: Sound) => {
+    const updatedSounds = sounds.map((s) =>
+      s.fullName === sound.fullName
+        ? {
+            ...s,
+            isLiked: !s.isLiked,
+            likes: s.isLiked ? s.likes - 1 : s.likes + 1,
+          }
+        : s
+    );
+    setSounds(updatedSounds);
+  };
+
+  const filteredSounds = sounds.filter((sound) =>
+    sound.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+  const sortedSounds = filteredSounds.sort((a, b) => {
+    // Ensure the values are treated as strings
+    const fieldA1 = String(a[sortBy[0] as keyof Sound]).toLowerCase();
+    const fieldB1 = String(b[sortBy[0] as keyof Sound]).toLowerCase();
+    if (fieldA1 !== fieldB1) {
+      return sortOrder[0] === 'asc'
+        ? fieldA1 < fieldB1
+          ? -1
+          : 1
+        : fieldA1 > fieldB1
+        ? -1
+        : 1;
+    }
+
+    const fieldA2 = String(a[sortBy[1] as keyof Sound]).toLowerCase();
+    const fieldB2 = String(b[sortBy[1] as keyof Sound]).toLowerCase();
+    return sortOrder[1] === 'asc'
+      ? fieldA2 < fieldB2
+        ? -1
+        : 1
+      : fieldA2 > fieldB2
+      ? -1
+      : 1;
   });
 
-  const SkeletonPlayButton = () => (
-    <div className="w-12 h-12 bg-gray-600 rounded-full animate-pulse mt-4" />
-  );
-  const SkeletonTitle = () => (
-    <div className="w-24 h-4 bg-gray-600 rounded-md animate-pulse mt-2" />
-  );
-  const SkeletonButton = () => (
-    <div className="w-20 h-8 bg-gray-600 rounded-md animate-pulse mt-4" />
-  );
+  const toggleSort = (field: 'name' | 'submittedBy') => {
+    if (sortBy[0] === field) {
+      const newSortOrder = sortOrder[0] === 'asc' ? 'desc' : 'asc';
+      setSortOrder([newSortOrder, sortOrder[1]]);
+    } else if (sortBy[1] === field) {
+      const newSortOrder = sortOrder[1] === 'asc' ? 'desc' : 'asc';
+      setSortOrder([sortOrder[0], newSortOrder]);
+    } else {
+      setSortBy([field, sortBy[0]]);
+      setSortOrder(['asc', sortOrder[1]]);
+    }
+  };
 
   return (
     <div className="flex min-h-screen bg-gradient-to-br from-zinc-900 to-zinc-800 text-white">
@@ -64,14 +223,15 @@ export default function Sounds() {
       <div className="flex-grow h-screen flex flex-col">
         <Spotlight />
 
-        <main className="flex-grow flex flex-col transition-all duration-300 mt-5">
-          <div className="flex flex-col items-center justify-center h-1/3">
-            <h1 className="font-extrabold text-3xl text-center text-white">
+        <main className="flex-grow flex flex-col transition-all duration-300 p-6 gap-8">
+          <div className="text-center">
+            <h1 className="font-extrabold text-4xl text-white tracking-tight">
               SOUNDS
             </h1>
           </div>
-          <div className="flex justify-center mb-4">
-            <div className="flex items-center gap-2 bg-purple-500/20 text-purple-500 px-4 py-2 rounded-full border border-purple-500/50 shadow-lg">
+
+          <div className="flex justify-center">
+            <div className="flex items-center gap-2 bg-purple-500/20 text-purple-500 px-4 py-2 rounded-full border border-purple-500/50 shadow-lg w-full max-w-md">
               <BiSearch className="text-md" />
               <input
                 type="text"
@@ -83,63 +243,156 @@ export default function Sounds() {
             </div>
           </div>
 
-          <div className="flex justify-center">
-            <div className="w-full max-w-5xl h-[600px] overflow-y-auto p-4 rounded-2xl">
-              <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6 p-4">
-                {loading
-                  ? Array(20)
-                      .fill(null)
-                      .map((_, index) => (
-                        <div
-                          key={index}
-                          className="group flex flex-col items-center justify-center p-6 rounded-lg shadow-xl bg-white/3 backdrop-blur-sm transition-all duration-300"
-                        >
-                          <SkeletonPlayButton />
-                          <SkeletonTitle />
-                          <SkeletonButton />
-                        </div>
-                      ))
-                  : filteredSounds.map((sound, index) => (
-                      <div
-                        key={index}
-                        className="group flex flex-col items-center justify-center p-6 rounded-lg shadow-xl bg-white/3 backdrop-blur-sm transition-all duration-300 border border-white hover:bg-white/10 hover:border-purple-400"
+          <div className="bg-zinc-800 p-6 rounded-xl shadow-lg">
+            <div className="overflow-auto max-h-[650px]">
+              <table className="w-full table-auto text-left text-sm border-separate border-spacing-y-2">
+                <thead>
+                  <tr className="text-gray-400 sticky top-0 z-10 backdrop-blur-lg">
+                    <th className="px-4 py-2">Play</th>
+                    <th
+                      className="px-4 py-2"
+                      onClick={() => toggleSort('name')}
+                    >
+                      Name
+                      {sortBy[0] === 'name' && sortOrder[0] === 'asc' ? (
+                        <FaSortAlphaUp className="inline ml-2" />
+                      ) : (
+                        <FaSortAlphaDown className="inline ml-2" />
+                      )}
+                    </th>
+                    <th
+                      className="px-4 py-2"
+                      onClick={() => toggleSort('submittedBy')}
+                    >
+                      Submitted By
+                      {sortBy[1] === 'submittedBy' && sortOrder[1] === 'asc' ? (
+                        <FaSortAlphaUp className="inline ml-2" />
+                      ) : (
+                        <FaSortAlphaDown className="inline ml-2" />
+                      )}
+                    </th>
+                    <th className="px-4 py-2">Likes</th>
+                    <th className="px-4 py-2">Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {loading ? (
+                    [...Array(20)].map((_, i) => (
+                      <tr key={i} className="bg-zinc-700 animate-pulse">
+                        <td className="px-4 py-2">
+                          <div className="w-8 h-8 bg-zinc-600 rounded-full mx-auto" />
+                        </td>
+                        <td className="px-4 py-2">
+                          <div className="h-4 w-32 bg-zinc-600 rounded" />
+                        </td>
+                        <td className="px-4 py-2">
+                          <div className="h-4 w-24 bg-zinc-600 rounded" />
+                        </td>
+                        <td className="px-4 py-2">
+                          <div className="h-4 w-16 bg-zinc-600 rounded" />
+                        </td>
+                        <td className="px-4 py-2">
+                          <div className="flex gap-4">
+                            <div className="w-8 h-8 bg-zinc-600 rounded-lg" />
+                            <div className="w-8 h-8 bg-zinc-600 rounded-lg" />
+                          </div>
+                        </td>
+                      </tr>
+                    ))
+                  ) : sortedSounds.length === 0 ? (
+                    <tr>
+                      <td
+                        colSpan={5}
+                        className="text-center py-4 text-gray-400"
                       >
-                        <button
-                          onClick={() => handlePlayPause(sound)}
-                          className="flex items-center justify-center m-3 text-purple-500 shadow-lg hover:text-purple-400 transition-all duration-300"
-                        >
-                          {playingSound === sound ? (
-                            <FaPause size={20} />
-                          ) : (
-                            <FaPlay size={20} />
-                          )}
-                        </button>
-
-                        {/* Sound Title */}
-                        <h3 className="text-md md:text-md font-semibold text-center text-white mt-2 truncate">
-                          {sound.replace('.ogg', '')}
-                        </h3>
-
-                        {/* Download Button */}
-                        <div className="mt-4 w-full flex justify-center">
-                          <a
-                            href={`/sounds/${sound}`}
-                            download
-                            className="opacity-0 group-hover:opacity-100 transition-all duration-300 bg-purple-500/20 text-purple-500 px-2 py-1 rounded-md border border-purple-500/50 shadow-lg hover:border-purple-500 m-1"
+                        No matching sounds found.
+                      </td>
+                    </tr>
+                  ) : (
+                    sortedSounds.map((sound) => (
+                      <tr
+                        key={sound.fullName}
+                        className="bg-zinc-700 transition-all duration-300 hover:bg-zinc-600"
+                      >
+                        <td className="px-4 py-2">
+                          <button
+                            onClick={() =>
+                              currentlyPlayingId === sound.fullName
+                                ? setCurrentlyPlayingId(null)
+                                : playSound(sound.fileUrl, sound.fullName)
+                            }
+                            className="text-purple-400 hover:bg-white/10 rounded-lg p-2 transition-all duration-300"
+                            aria-label={`Play ${sound.name}`}
                           >
-                            <LuDownload size={20} />
-                          </a>
-                        </div>
-                      </div>
-                    ))}
-              </div>
+                            {currentlyPlayingId === sound.fullName ? (
+                              <FaPause className="text-xl w-4 h-4" />
+                            ) : (
+                              <FaPlay className="text-xl w-4 h-4" />
+                            )}
+                          </button>
+                        </td>
+                        <td className="px-4 py-2">{sound.name}</td>
+                        <td className="px-4 py-2 text-purple-300">
+                          {sound.submittedBy}
+                        </td>
+                        <td className="px-4 py-2">
+                          <button
+                            onClick={() => handleLikeToggle(sound)}
+                            className="flex items-center gap-2"
+                          >
+                            <FaHeart
+                              className={`w-5 h-5 transition-all ${
+                                sound.isLiked ? 'text-red-500' : 'text-white'
+                              }`}
+                            />
+                            <span>{sound.likes}</span>
+                          </button>
+                        </td>
+                        <td className="px-4 py-2">
+                          <div className="flex gap-4 items-center">
+                            <button
+                              onClick={() => handleDownload(sound.fileUrl)}
+                              className="text-white hover:bg-white/10 rounded-lg p-2 transition-all duration-300"
+                              aria-label={`Download ${sound.name}`}
+                            >
+                              <FaDownload className="text-xl w-4 h-4" />
+                            </button>
+                            <button
+                              onClick={() => handleDelete(sound)}
+                              className="text-white hover:bg-white/10 rounded-lg p-2 transition-all duration-300"
+                              aria-label={`Delete ${sound.name}`}
+                            >
+                              <FaTrash className="text-red-500 text-xl w-4 h-4" />
+                            </button>
+                          </div>
+                        </td>
+                      </tr>
+                    ))
+                  )}
+                </tbody>
+              </table>
             </div>
           </div>
 
-          <div className="px-6 mt-auto">
-            <Footer />
-          </div>
+          <Toast
+            message={toast.message}
+            type={toast.type}
+            isVisible={toast.isVisible}
+            onClose={handleCloseToast}
+          />
+
+          <ConfirmDialog
+            isOpen={isConfirmDialogOpen}
+            message={`Are you sure you want to delete the sound "${soundToDelete?.name}.ogg"?`}
+            onConfirm={confirmDelete}
+            onCancel={cancelDelete}
+            title="Confirm Deletion"
+          />
         </main>
+
+        <div className="px-6">
+          <Footer />
+        </div>
       </div>
     </div>
   );
