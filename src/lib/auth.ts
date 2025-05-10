1b4952353011d7dc54d34b545bcd602240a573aa
pyvnoaim <pyvnovlr@gmail.com>
2025-05-10 20:01:02 +0200

    removed auth debugging

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -46,5 +46,4 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     strategy: 'database',
     maxAge: 30 * 24 * 60 * 60, // 30 days
   },
-  debug: process.env.NODE_ENV === 'development',
 })
