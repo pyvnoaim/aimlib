9e392c5d6f0bd4fa5ca9e1831fb8fb1622d711c1
pyvnoaim <pyvnovlr@gmail.com>
2025-05-10 19:49:10 +0200

    updated auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,20 +1,10 @@
-import NextAuth from 'next-auth';
-import { DrizzleAdapter } from '@auth/drizzle-adapter';
-import { db } from '@/db/index';
-import { users } from '@/db/schema/users';
-import { eq } from 'drizzle-orm';
-import Discord from 'next-auth/providers/discord';
-import { ROLES } from '@/types/role';
-
-declare module 'next-auth' {
-  interface User {
-    role?: string;
-  }
-
-  interface Session {
-    user: User;
-  }
-}
+import NextAuth from 'next-auth'
+import { DrizzleAdapter } from '@auth/drizzle-adapter'
+import { db } from '@/db/index'
+import { users } from '@/db/schema/users'
+import { eq } from 'drizzle-orm'
+import Discord from 'next-auth/providers/discord'
+import { ROLES, Role } from '@/types/role'
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   trustHost: true,
@@ -33,24 +23,23 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
           .select()
           .from(users)
           .where(eq(users.id, user.id))
-          .then((res) => res[0]);
+          .then((res) => res[0])
 
         if (dbUser) {
-          session.user.id = dbUser.id;
+          session.user.id = dbUser.id
+          if (dbUser.email) session.user.email = dbUser.email
+          if (dbUser.name) session.user.name = dbUser.name
+          if (dbUser.image) session.user.image = dbUser.image
 
-          if (dbUser.email) session.user.email = dbUser.email;
-          if (dbUser.name) session.user.name = dbUser.name;
-          if (dbUser.image) session.user.image = dbUser.image;
-
-          session.user.role = dbUser.role ?? ROLES.USER;
+          session.user.role = dbUser.role ?? ROLES.USER
         } else {
-          console.error('User not found in database');
+          console.error('User not found in database')
         }
       } catch (error) {
-        console.error('Error fetching user from database:', error);
+        console.error('Error fetching user from database:', error)
       }
 
-      return session;
+      return session
     },
   },
   session: {
@@ -58,4 +47,4 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     maxAge: 30 * 24 * 60 * 60, // 30 days
   },
   debug: process.env.NODE_ENV === 'development',
-});
+})
