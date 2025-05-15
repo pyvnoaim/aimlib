29f42c94f94ec77db5043c64c12a70007729d1e5
pyvnoaim <pyvnovlr@gmail.com>
2025-05-15 15:37:06 +0200

    cuter footer msg

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { FaDiscord } from 'react-icons/fa6';
+import { FaDiscord, FaGithub } from 'react-icons/fa';
 
 const Footer: React.FC = () => {
   return (
     <footer className="hidden sm:flex w-full justify-center items-center px-8 pb-4 z-10">
       <div className="bg-zinc-800 rounded-lg shadow-lg text-center w-full flex items-center justify-center py-3 relative border border-zinc-700">
         <p className="text-white">
-          AIMLIB ─ made by{' '}
+          made with <span className="text-red-500 animate-pulse">❤︎</span> by{' '}
           <a
             href="https://x.com/pyvnoaim"
             target="_blank"
@@ -43,7 +43,15 @@ const Footer: React.FC = () => {
             @shishigami
           </a>
         </p>
-        <div className="flex space-x-4 absolute right-6">
+        <div className="flex space-x-4 absolute right-4">
+          <a
+            href="https://github.com/pyvnoaim/aimlib"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-white hover:bg-zinc-700 rounded-md transition duration-300 p-2"
+          >
+            <FaGithub size={20} />
+          </a>
           <a
             href="https://discord.gg/w6AvmXwXwH"
             target="_blank"
