4a80d915f598ee2fb8b99a8ee8c0f811401116eb
pyvnoaim <pyvnovlr@gmail.com>
2025-06-17 22:01:16 +0200

    added test table

--- src/app/(pages)/playlists/page.tsx
+++ src/app/(pages)/playlists/page.tsx
@@ -1,25 +1,41 @@
 'use client';
-
-import { BiSolidTime } from 'react-icons/bi';
 import Footer from '@/components/footer';
 import Background from '@/components/background';
 
-export default function Valorant() {
+export default function Playlists() {
   return (
-    <div className="flex flex-grow flex-col min-h-screen bg-zinc-900 text-white">
+    <div className="flex min-h-screen bg-zinc-900 text-white">
       <Background />
-      <div className="flex flex-col items-center justify-center flex-1 space-y-8 relative z-10 p-8">
-        <h1 className="font-extrabold text-5xl md:text-6xl text-center text-white">
-          PLAYLISTS
-        </h1>
-        <div className="flex justify-center">
-          <div className="flex items-center gap-2 bg-purple-500/20 text-purple-400 px-3 py-1.5 rounded-full border border-purple-500/50 shadow-lg backdrop-blur-sm transition-all duration-300">
-            <BiSolidTime className="text-base" />
-            <span className="text-base font-medium">coming soon</span>
+
+      <div className="flex-grow h-screen flex flex-col z-10 relative">
+        <header className="pt-6">
+          <div className="text-center">
+            <h1 className="font-extrabold text-5xl md:text-6xl text-white">
+              PLAYLISTS
+            </h1>
           </div>
-        </div>
+        </header>
+
+        <main className="flex-grow flex flex-col transition-all duration-300 p-8">
+          <section className="bg-zinc-800 p-4 h-full rounded-lg shadow-lg border border-zinc-700">
+            <table className="w-full overflow-auto">
+              <thead>
+                <tr className="uppercase text-sm text-zinc-400 sticky top-0 bg-zinc-800/50 backdrop-blur-sm">
+                  <th className="p-2 text-left">Play</th>
+                  <th className="p-2 text-center">Name</th>
+                  <th className="p-2 text-center">Author</th>
+                  <th className="p-2 text-center">Aimtrainer</th>
+                  <th className="p-2 text-center">Likes</th>
+                  <th className="p-2 text-center">Playtime</th>
+                  <th className="p-2 text-right">Actions</th>
+                </tr>
+              </thead>
+            </table>
+          </section>
+        </main>
+
+        <Footer />
       </div>
-      <Footer />
     </div>
   );
 }
