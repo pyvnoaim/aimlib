fb74b65d47465d2c1371829d36c08d58cf02e7ea
pyvnoaim <pyvnovlr@gmail.com>
2025-06-24 20:30:44 +0200

    removed link column

--- src/db/schema/playlists.ts
+++ src/db/schema/playlists.ts
@@ -13,6 +13,5 @@ export const playlists = mysqlTable('playlists', {
   author: varchar('author', { length: 255 }).notNull(),
   aimtrainer: mysqlEnum('aimtrainer', ["KovaaK's", 'Aimlabs']),
   shareCode: varchar('share_code', { length: 255 }),
-  link: varchar('link', { length: 1000 }).notNull(),
   createdAt: timestamp('created_at', { mode: 'date', fsp: 3 }).defaultNow(),
 });
