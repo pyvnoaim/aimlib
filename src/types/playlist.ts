17451da13810a4f298b90d37bfc30ac9c004b324
pyvnoaim <pyvnovlr@gmail.com>
2025-06-25 11:46:15 +0200

    added playlist types

---
+++ src/types/playlist.ts
@@ -0,0 +1,10 @@
+export type Playlist = {
+  id: string;
+  name: string;
+  author: string;
+  aimtrainer: string;
+  shareCode: string;
+  likes: number;
+  createdAt: string;
+  likedByUser: boolean;
+};
