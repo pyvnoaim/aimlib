e0476840f30264184d6df6ba70017814ad25c033
pyvnoaim <pyvnovlr@gmail.com>
2025-07-01 10:04:14 +0200

    updated metadata

--- src/app/(pages)/playlists/layout.tsx
+++ src/app/(pages)/playlists/layout.tsx
@@ -11,8 +11,6 @@ export const metadata: Metadata = {
     images: [
       {
         url: 'https://aimlib.xyz/logo.png',
-        width: 1200,
-        height: 630,
         alt: 'AIMLIB | Playlists',
       },
     ],
