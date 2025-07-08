cbdf708cb05dc584c95a0dcd3b1a6069addd16d9
pyvnoaim <pyvnovlr@gmail.com>
2025-07-08 14:00:29 +0200

    updated playlist schema

--- src/db/schema/playlists.ts
+++ src/db/schema/playlists.ts
@@ -3,17 +3,18 @@ import {
   varchar,
   timestamp,
   mysqlEnum,
+  boolean,
 } from 'drizzle-orm/mysql-core';
 
 export const playlists = mysqlTable('playlists', {
-  id: varchar('id', { length: 255 })
-    .primaryKey()
-    .$defaultFn(() => crypto.randomUUID()),
+  id: varchar('id', { length: 255 }).primaryKey(),
   name: varchar('name', { length: 255 }).notNull(),
   author: varchar('author', { length: 255 }).notNull(),
   twitterHandle: varchar('twitterHandle', { length: 255 }).notNull(),
   aimtrainer: mysqlEnum('aimtrainer', ["KovaaK's", 'Aimlabs']).notNull(),
   shareCode: varchar('shareCode', { length: 255 }).unique(),
+  isBenchmark: boolean('isBenchmark').default(false).notNull(),
+  benchmarkLink: varchar('benchmarkLink', { length: 2048 }),
   createdAt: timestamp('createdAt', { mode: 'date', fsp: 3 }).defaultNow(),
   updatedAt: timestamp('updatedAt', { mode: 'date', fsp: 3 })
     .defaultNow()
