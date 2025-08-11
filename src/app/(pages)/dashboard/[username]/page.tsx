fb9e1cce257f023c207303831a9b4987061829fd
pyvnoaim <pyvnovlr@gmail.com>
2025-08-11 16:09:28 +0200

    fixed view benchmark position

--- src/app/(pages)/dashboard/[username]/page.tsx
+++ src/app/(pages)/dashboard/[username]/page.tsx
@@ -258,28 +258,28 @@ export default function UserDashboard() {
                       </a>
                     </p>
                   )}
-                </div>
-                <a
-                  href={benchmark.benchmarkLink}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="inline-flex items-center gap-2 hover:text-purple-400 text-sm underline mb-4 transition-all duration-300"
-                >
-                  View Benchmark
-                  <svg
-                    className="w-4 h-4"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
+                  <a
+                    href={benchmark.benchmarkLink}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="inline-flex items-center gap-2 hover:text-purple-400 text-sm underline transition-all duration-300"
                   >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
-                    />
-                  </svg>
-                </a>
+                    View Benchmark
+                    <svg
+                      className="w-4 h-4"
+                      fill="none"
+                      stroke="currentColor"
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+                      />
+                    </svg>
+                  </a>
+                </div>
               </>
             ) : (
               <p className="text-gray-400">
